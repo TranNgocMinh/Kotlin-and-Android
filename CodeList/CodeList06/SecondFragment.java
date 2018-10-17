8c832ad5b5f87b96b75f61635ead46643b74f8d1
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-10-17 09:25:47 +0700

    Create SecondFragment.java

---
+++ CodeList/CodeList06/SecondFragment.java
@@ -0,0 +1,27 @@
+package com.ngocminhtran.fragmentexample;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SecondFragment extends Fragment  {
+
+    private static TextView textview;
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_second_fragment, container, false);
+
+        textview = (TextView) view.findViewById(R.id.textView1);
+
+        return view;
+    }
+    public void changeTextProperties(int fontsize, String text)
+    {
+        textview.setTextSize(fontsize);
+        textview.setText(text);
+    }
+}
