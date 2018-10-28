2e6f144282fb59a61dee6cdcd21283f394293215
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-10-28 10:29:58 +0700

    Create ActivityB.java

---
+++ CodeList/CodeList08/ActivityB.java
@@ -0,0 +1,41 @@
+package com.ngocminhtran.explicitintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+
+        String qString = extras.getString("qString");
+        final TextView textView = (TextView)
+                    findViewById(R.id.textView1);
+        textView.setText(qString);
+    }
+
+    @Override
+    public void finish() {
+        Intent data = new Intent();
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        String returnString = editText1.getText().toString();
+        data.putExtra("returnData", returnString);
+        setResult(RESULT_OK, data);
+        super.finish();
+    }
+
+    public void onClick(View view) {
+        finish();
+    }
+}
