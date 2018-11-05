42c21a09e4b433f5d97b217ff09504c3f070d8c6
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-11-05 07:26:21 +0700

    Create MyReceiver.java

---
+++ CodeList/CodeList10/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.ngocminhtran.sendbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String message = "Broadcast intent detected "
+                + intent.getAction();
+        Toast.makeText(context, message,
+                Toast.LENGTH_LONG).show();
+    }
+}
