845cf46d71b26bd57fcf969706970c3ca0098aad
Ngoc Minh <tranngocminhcdn@gmail.com>
2020-05-27 19:59:42 +0700

    Create User.java

---
+++ CodeList/CodeList18/User.java
@@ -0,0 +1,10 @@
+package com.example.mydatabinding;
+
+public class User {
+    public final String fullName;
+    public final Integer age;
+    public User(String fullName, Integer age){
+        this.fullName = fullName;
+        this.age = age;
+    }
+}
